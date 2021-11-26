aee5dcc3ac0c7e6f105fdf6256fc7357ffd74bd8
Raquel <raquel@parcellab.com>
2021-11-26 09:04:44 +0100

    import boostrap in another way

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Application } from './App'
 import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/styles/index.scss'
 
 function renderReturnsPlugin () {
